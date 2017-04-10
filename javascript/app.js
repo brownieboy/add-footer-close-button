56516cf0d58c71c957c0c0d57160f02af9d1bc16
Michael Brown <brownieboy@gmail.com>
2017-04-10 20:33:34 +1000

    Added 1 sec delay to work with Hashnode

--- javascript/app.js
+++ javascript/app.js
@@ -5,18 +5,29 @@
  */
 
 $(function() {
-  var $bottomBar = $(".u-fixed.u-bottom0");
-  if ($bottomBar.length > 0) {
-    $bottomBar = $(".jn-content");
-  }
-  if ($bottomBar.length > 0) {
-    var $bottomBarFirstChild = $bottomBar.find(">:first-child");
-    console.log("Medium style bar found, adding close button...");
-    var closeImg = chrome.extension.getURL("images/high-contrast-dialog-close.svg");
-    $bottomBarFirstChild.append('<style type="text/css">#mb-footer-close-button:hover {cursor:pointer}</style><img id="mb-footer-close-button" src="' + closeImg + '" style="height:16px; width:16px;margin-left:5px;" alt="Close footer" title="Close footer">');
 
-    $("#mb-footer-close-button").click(function() {
-      $bottomBar.hide("slow");
-    });
-  }
+  setTimeout(function() {
+    var $bottomBar = $(".u-fixed.u-bottom0"); // Medium style
+    var $appendTarget = $bottomBar.find(">:first-child");
+    if ($bottomBar.length > 0) {
+      console.log("Mediaum style found");
+    }
+
+    if ($bottomBar.length === 0) {
+
+      $bottomBar = $(".jn-content"); // Hashnode
+      if ($bottomBar.length > 0) {
+        $appendTarget = $bottomBar.find("> .col-2");
+        console.log("Hashnode style found");
+      }
+    }
+    if ($bottomBar.length > 0) {
+      console.log("Adding to element");
+      var closeImg = chrome.extension.getURL("images/high-contrast-dialog-close.svg");
+      $appendTarget.append('<style type="text/css">#mb-footer-close-button:hover {cursor:pointer}</style><img id="mb-footer-close-button" src="' + closeImg + '" style="height:16px; width:16px;margin-left:5px;" alt="Close footer" title="Close footer">');
+      $("#mb-footer-close-button").click(function() {
+        $bottomBar.hide("slow");
+      });
+    }
+  }, 1000);
 });
